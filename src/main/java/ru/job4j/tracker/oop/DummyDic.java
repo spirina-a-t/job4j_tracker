b655dc4cbe03215cabbb294fb85f624769360b64
spirina-a-t <[tatyananavis@gmail.com]>
2021-02-12 13:10:46 +0300

    1.4. Вызов метода с возвращаемым типом. [#426798]

---
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        String result = "Неизвествное слово. ";
+        return result + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic word = new DummyDic();
+        System.out.println(word.engToRus("How"));
+    }
+}
