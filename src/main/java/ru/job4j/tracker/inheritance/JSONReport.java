9dccb5a07818def8f82c729a76adfd2b148451eb
spirina-a-t <[tatyananavis@gmail.com]>
2021-02-17 00:04:54 +0300

    4. Переопределение [#426806]

---
+++ src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator()
+                + name + " : \"name\"," + System.lineSeparator()
+                + body + " : \"body\"" + System.lineSeparator() + "}";
+    }
+}
