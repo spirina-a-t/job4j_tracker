a5b74dc8ad180ef67274bef5f6d064db4b4bd3cb
spirina-a-t <[tatyananavis@gmail.com]>
2021-02-20 14:39:31 +0300

    3. Массивы и модели. [#426814]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,29 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book first = new Book("Clean Code", 464);
+        Book second = new Book("Agile Testing", 576);
+        Book third = new Book("Hibernate", 382);
+        Book four = new Book("Head First Servlets", 914);
+        Book[] book = {first, second, third, four};
+        for (int i = 0; i < book.length; i++) {
+            Book temp = book[i];
+            System.out.println(temp.getTitle() + " - " + temp.getPages());
+        }
+        System.out.println("Swap the first and last book:");
+        Book tmp = book[0];
+        book[0] = book[3];
+        book[3] = tmp;
+        for (int i = 0; i < book.length; i++) {
+            Book temp = book[i];
+            System.out.println(temp.getTitle() + " - " + temp.getPages());
+        }
+        System.out.println("Only books with a title \"Clean Code\":");
+        for (Book temp : book) {
+            if (temp.equals(book[3])) {
+                System.out.println(temp.getTitle() + " - " + temp.getPages());
+            }
+        }
+    }
+}
